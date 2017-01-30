3b9e8d0eef701da95e37625fbd34216d17e83237
home <home>
2017-01-31 00:08:53 +0900

    camera2 api change

--- app/src/main/java/com/madfish/james/findyourvision/MainActivity.java
+++ app/src/main/java/com/madfish/james/findyourvision/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,99 +24,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    CameraDevice mCameraDevice;
+    private TextureView mCameraTextureView;
+    private Preview mPreview;
+    static final int REQUEST_CAMERA = 1;
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,  String[] permissions,int[] grandResults){
-        boolean allowed = true;
+    private Preview preview;
 
-        switch(requestCode){
-            //camera access storage access
-            case  CameraDevice.REQUEST_CODE_STORAGE_PERM:
-                for(int res : grandResults){
-                    allowed = allowed && (res == PackageManager.PERMISSION_GRANTED);
-                    Toast.makeText(this, "Camera Permission Granted", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-                Toast.makeText(this,"Camera Permission not Granted" , Toast.LENGTH_SHORT).show();
-                break;
-        }
-        if(allowed){
-            doRestart(this);
-        }else{//what is this?
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-                    Toast.makeText(MainActivity.this, "Camera Permissions denied", Toast.LENGTH_SHORT).show();
-                }
-                else if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
-                    Toast.makeText(MainActivity.this, "Storage Permissions denied", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    } public static void doRestart(Context c) {
-        //http://stackoverflow.com/a/22345538
-        try {
-            //check if the context is given
-            if (c != null) {
-                //fetch the packagemanager so we can get the default launch activity
-                // (you can replace this intent with any other activity if you want
-                PackageManager pm = c.getPackageManager();
-                //check if we got the PackageManager
-                if (pm != null) {
-                    //create the intent with the default start activity for your application
-                    Intent mStartActivity = pm.getLaunchIntentForPackage(
-                            c.getPackageName()
-                    );
-                    if (mStartActivity != null) {
-                        mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        //create a pending intent so the application is restarted after System.exit(0) was called.
-                        // We use an AlarmManager to call this intent in 100ms
-                        int mPendingIntentId = 223344;
-                        PendingIntent mPendingIntent = PendingIntent
-                                .getActivity(c, mPendingIntentId, mStartActivity,
-                                        PendingIntent.FLAG_CANCEL_CURRENT);
-                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
-                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
-                        //kill the application
-                        System.exit(0);
-                    } else {
-                        Log.e("permission", "Was not able to restart application, mStartActivity null");
-                    }
-                } else {
-                    Log.e("permission", "Was not able to restart application, PM null");
-                }
-            } else {
-                Log.e("permission", "Was not able to restart application, Context null");
-            }
-        } catch (Exception ex) {
-            Log.e("permission", "Was not able to restart application");
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        setContentView(R.layout.activity_main);
-        // setting camera functions
-        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-            if (!mCameraDevice.hasPermissions()) {
-                // your app doesn't have permissions, ask for them.
-                mCameraDevice.requestNecessaryPermissions();
-            } else {
-                // your app already have permissions allowed.
-                // do what you want.
-                //startCamera();
-            }
-        } else {
-            Toast.makeText(MainActivity.this, "Camera not supported", Toast.LENGTH_LONG).show();
-        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -137,11 +58,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
+
+        mCameraTextureView = (TextureView) findViewById(R.id.cameraTextureView);
+        mPreview = new Preview(this, mCameraTextureView);
+ }
     @Override
     protected void onResume(){
         super.onResume();
-        mCameraDevice.startCamera();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
